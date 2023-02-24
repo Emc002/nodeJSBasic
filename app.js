636e972fb032c23ec8f6d0b69b4039ddee2eb335
Emc002 <alifnafisalghany@gmail.com>
2023-02-24 13:35:28 +0700

    secondCommit

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
-// npm - global command, come with node
-// npm --version
+var http = require('http')
+var fs = require('fs')
 
-// local depepndency - use it only in thid particular project
-// npm i <packageName>
-
-// global dependency - use it in any project
-// npm install -g <packageName>
-// sudo npm install -g <packageName> (mac)
-
-// package.json - manifest file (stores important into about project/package)
-// manual approach (create package.json in the root, create properties etc)
-// npm init (step by step, press enter to skip)
-// npm init -y (everything default)
-
-const _ = require('lodash');
-
-const items =[1, [2, [3, [4, [5]]]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems)
-
+http
+  .createServer(function (req, res) {
+    // const text = fs.readFileSync('./content/big.txt', 'utf8')
+    // res.end(text)
+    const fileStream = fs.createReadStream('./content/big.txt', 'utf8')
+    fileStream.on('open', () => {
+      fileStream.pipe(res)
+    })
+    fileStream.on('error', (err) => {
+      res.end(err)
+    })
+  })
+  .listen(5000)
